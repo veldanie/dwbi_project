14656aecc7365b1ac851bea7d9d42f89536021cf
ivan-vallejo <ivan.vallejo@barcelonagse.eu>
2016-11-10 00:07:26 +0100

    Added Google price tablesto telecom.sql

--- db/telecom.sql
+++ db/telecom.sql
@@ -1,8 +1,8 @@
--- Data Warehousing and Cumputil Lab Project 
+-- Data Warehousing and Cumputer Lab Project 
 
 -- Create Database
 
-drop database telecom;
+drop database if exists telecom;
 create database telecom;
 
 -- select the database
@@ -10,7 +10,7 @@ create database telecom;
 use telecom;
 
 -- create the required tables
-
+drop table if exists wb_indicators;
 create table wb_indicators (
 
 IndicatorCode nchar(13) not null,
@@ -19,15 +19,40 @@ Description varchar(4000),
 primary key (IndicatorCode)
 );
 
+drop table if exists wb_data;
 create table wb_data (
-Country nchar(10) not null,
+Country nchar(15) not null,
 IndicatorCode nvarchar(13) not null,
-Year int not null,
-Value double,
+`Year` int not null,
+`Value` double,
 
-primary key (Country, IndicatorCode, Year),
+primary key (Country, IndicatorCode, `Year`),
 foreign key (IndicatorCode) references wb_indicators (IndicatorCode) on delete no action on update no action
 );
 
+drop table if exists fixedbb_prices;
+create table fixedbb_prices(
+Country nchar(15) not null,
+`Year` int not null,
+Price numeric(10,3) not null,
+Speed numeric(10,3) not null,
+Cap nvarchar(15) not null,
+Operator nvarchar(70) not null,
 
+primary key (Country, `Year`, Operator, Price)  
+); 
+
+
+drop table if exists mobilebb_prices;
+create table mobilebb_prices(
+Country nchar(15) not null,
+`Year` int not null,
+Price numeric(10,3) not null,
+Cap nvarchar(15) not null,
+Validity int not null,
+Contract nvarchar(15) not null,  
+Operator nvarchar(70) not null,
+
+primary key (Country, `Year`, Operator, Price)  
+) ;
 -- end of file
