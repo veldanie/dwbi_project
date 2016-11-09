02a0e4cbe8e17c392b97a4f0cfa0c99fc3b8eac3
veldanie <davelasquez@gmail.com>
2016-11-10 00:52:46 +0100

    CountryCodes and ITU table

--- db/telecom.sql
+++ db/telecom.sql
@@ -10,23 +10,56 @@ create database telecom;
 use telecom;
 
 -- create the required tables
+-- Countries
+create table countries (
 
+Country nchar(10) not null,
+CountryCode int not null,
+
+primary key (CountryCode)
+);
+
+
+-- World Bank Data
 create table wb_indicators (
 
-IndicatorCode nchar(13) not null,
+IndicatorCode nchar(20) not null,
 Description varchar(4000),
 
 primary key (IndicatorCode)
 );
 
 create table wb_data (
-Country nchar(10) not null,
+CountryCode int not null,
 IndicatorCode nvarchar(13) not null,
-Year int not null,
-Value double,
+IndicatorYear int not null,
+IndicatorValue double,
 
-primary key (Country, IndicatorCode, Year),
+primary key (CountryCode, IndicatorCode, IndicatorYear),
 foreign key (IndicatorCode) references wb_indicators (IndicatorCode) on delete no action on update no action
+foreign key (CountryCode) references countries (CountryCode) on delete no action on update no action
+
+);
+
+-- Itu Indicators
+create table itu_indicators (
+
+IndicatorCode nchar(20) not null,
+Description varchar(4000),
+
+primary key (IndicatorCode)
+);
+
+create table itu_data (
+CountryCode int not null,
+IndicatorCode nvarchar(13) not null,
+IndicatorYear int not null,
+IndicatorValue double,
+
+primary key (CountryCode, IndicatorCode, IndicatorYear),
+foreign key (IndicatorCode) references itu_indicators (IndicatorCode) on delete no action on update no action
+foreign key (CountryCode) references countries (CountryCode) on delete no action on update no action
+
 );
 
 
