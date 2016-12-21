aa183aec1f6300f53971552bf05d1460e5c4d0aa
veldanie <davelasquez@gmail.com>
2016-12-21 22:26:06 +0100

    website footer

--- website-project/footer.php
+++ website-project/footer.php
@@ -1,3 +1,6 @@
-		</section>
-	</body>
-</html>
+<footer>
+  <br><br>
+  Ivan Vallejo Vall <a href="mailto:ivan.vallejo@barcelonagse.eu">ivan.vallejo@barcelonagse.eu</a><br/>
+  Saurav Poudel <a href="mailto:saurav.poudel@barcelonagse.eu">saurav.poudel@barcelonagse.eu</a><br/>
+  Daniel Velásquez Vergara <a href="mailto:daniel.velasquez@barcelonagse.eu">daniel.velasquez@barcelonagse.eu</a>
+ </footer>
